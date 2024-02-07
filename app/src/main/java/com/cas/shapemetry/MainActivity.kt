26ef83d6723ba0fdc4ae8863df494eeff1a9a732
veroiconica.git <cinthia2117@gmail.com>
2024-02-07 16:25:37 -0500

    Splash screen created with the application icon in the center

--- app/src/main/java/com/cas/shapemetry/MainActivity.kt
+++ app/src/main/java/com/cas/shapemetry/MainActivity.kt
@@ -2,10 +2,26 @@ package com.cas.shapemetry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create a new instance of the BlankFragment
+        val blankFragment = SplashScreenFragment()
+
+        // Begin the transaction
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        // Add the BlankFragment on top of the MainActivity
+        fragmentTransaction.add(android.R.id.content, blankFragment)
+        fragmentTransaction.addToBackStack(null) // Add to back stack, so it can be removed later
+
+        // Commit the transaction
+        fragmentTransaction.commit()
     }
 }
